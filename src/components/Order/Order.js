fb004706d60b04b03260b5ef52c3ceb83a804023
Angela <angela.inniss@justpark.com>
2020-05-01 14:54:23 +0100

    fix up order page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,11 @@
 import React from "react";
+
 import classes from "./Order.css";
+import Burger from "../Burger/Burger";
+
 
 const Order = props => {
+
   const ingredients = [];
   for (let ingredientName in props.ingredients) {
     ingredients.push({
@@ -15,16 +19,7 @@ const Order = props => {
 
   const ingredientOutput = ingredients.map(ig => {
     return (
-      <span
-        style={{
-          textTransform: "capitalize",
-          display: "inline-block",
-          margin: "0 8px",
-          border: "1px solid #ccc",
-          padding: "5px"
-        }}
-        key={ig.name}
-      >
+      <span className={classes.Box} key={ig.name}>
         {ig.name} ({ig.amount})
       </span>
     );
@@ -33,7 +28,8 @@ const Order = props => {
   return (
     <div className={classes.Order}>
       <p>{ingredientOutput}</p>
-      <p> £{props.price.toFixed(2)}</p>
+      <Burger ingredients={props.ingredients}/>
+      <p><b> £{props.price.toFixed(2)}</b></p>
     </div>
   );
 };
