a7a9442a98df803be2a759f08da12199aabeb64c
Angela <angela.inniss@justpark.com>
2020-05-15 10:17:57 +0100

    update logout with hooks

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
-import * as actions from "../../../store/actions/index";
+import React, { useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
-class Logout extends Component {
-  // componentDid mount will be executed as soon as we enter page/render this component
-  componentDidMount() {
-    this.props.onLogout();
-  }
+import * as actions from "../../../store/actions/index";
 
-  render() {
-    return <Redirect to="/" />;
-  }
-}
+const logout = props => {
+  // componentDid mount will be executed as soon as we enter page/render this component
+  // componentDidMount() {
+  //   this.props.onLogout();
+  // }
+  useEffect(() => {
+    props.onLogout();
+  }, []); // runs on first mount only []
+
+  return <Redirect to="/" />;
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     onLogout: () => dispatch(actions.logout())
   };
 };
-export default connect(null, mapDispatchToProps)(Logout);
+export default connect(null, mapDispatchToProps)(logout);
